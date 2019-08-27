d57ae8ac606bdf9569a7a798f166cda1164b20ec
Esa Palosaari <esa.palosaari@gmail.com>
2019-08-27 20:35:54 +0300

    initialize

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+let numbers = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-532532",
+        id: 4
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 5
+    }
+]
+
+
+console.log('first iteration')
